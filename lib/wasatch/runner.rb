84ad335d8ff8f647df4e2574985117328e0a135a
McKay Ward <mckaykward@gmail.com>
2015-09-15 20:39:10 -0500

    basic skeleton for Runner class

---
+++ lib/wasatch/runner.rb
@@ -0,0 +1,7 @@
+class Runner
+  
+  def initialize(number, name=nil)
+    @number = number
+    @name   = name
+  end
+end
---
+++ spec/wasatch/runner_spec.rb
@@ -0,0 +1,7 @@
+require 'wasatch/runner'
+describe Runner do
+
+  before do
+    @runner = Runner.new(221)
+  end
+end
